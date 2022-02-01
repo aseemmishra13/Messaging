166d15ddf5d5661d4e9fb76844fa111abb67a50c
aseemmishra13 <mishraaseem05@gmail.com>
2022-02-01 10:33:55 +0530

    build

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const io = require('socket.io')(process.env.PORT || 5000,{
     cors: {
-            origin: ['https://sastawhatsapp.herokuapp.com/'],
+            origin: true,
+            
      },
   })
 
